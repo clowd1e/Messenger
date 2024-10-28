20c74ccc9ed60c6f50dffd913f222357e31027b5
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-10-28 11:43:48 +0100

    Implement Primitives

---
+++ Api/src/Messenger.Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿namespace Messenger.Domain.Primitives
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id) { }
+    }
+}
