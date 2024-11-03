71c82d6fd7eccdfda4315c0a128c0efb915e8822
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-03 20:05:22 +0100

    Add application layer

---
+++ Api/src/Messenger.Application/Extensions/DI/MapperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Messenger.Application.Abstractions.Data;
+using Messenger.Application.Features.Users.DTO;
+using Messenger.Application.Features.Users.Mappers;
+using Messenger.Domain.Aggregates.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger.Application.Extensions.DI
+{
+    public static class MapperExtensions
+    {
+        public static IServiceCollection AddMappers(this IServiceCollection services)
+        {
+            #region Users
+            services.AddMapper<User, UserResponse, UserToUserResponseMapper>();
+            #endregion
+            return services;
+        }
+        private static IServiceCollection AddMapper<TSource, TDestination, TMapper>(
+            this IServiceCollection services)
+            where TSource : class
+            where TDestination : class
+            where TMapper : Mapper<TSource, TDestination>
+        {
+            services.AddScoped<Mapper<TSource, TDestination>, TMapper>();
+            return services;
+        }
+    }
+}
---
+++ Api/src/Messenger.Application/Extensions/DI/ValidatorsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Messenger.Application.Extensions.DI
+{
+    public static class ValidatorsExtensions
+    {
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(
+                Assembly.GetExecutingAssembly(),
+                includeInternalTypes: true);
+
+            return services;
+        }
+    }
+}
