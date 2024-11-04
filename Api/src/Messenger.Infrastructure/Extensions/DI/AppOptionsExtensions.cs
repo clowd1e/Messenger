493ad876624ff4493fea88e0c20fdafbfd3082a7
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-04 12:58:14 +0100

    Add DependencyInjection

---
+++ Api/src/Messenger.Infrastructure/Extensions/DI/AppOptionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Messenger.Infrastructure.Extensions.DI.Shared;
+using Messenger.Infrastructure.Persistense.Options;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger.Infrastructure.Extensions.DI
+{
+    public static class AppOptionsExtensions
+    {
+        public static IServiceCollection AddAppOptions(
+            this IServiceCollection services)
+        {
+            services.ConfigureValidatableOnStartOptions<DbSettings>(
+                DbSettings.SectionName);
+
+            return services;
+        }
+    }
+}
