71162cd2861d93d6fb1f89b90db298ff44522019
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-19 14:39:24 +0100

    Add Register endpoint

---
+++ Api/src/Messenger.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Messenger.Application.Features.Auth.Commands.Register;
+using Messenger.WebAPI.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.WebAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public AuthController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(
+            [FromBody] RegisterCommand command,
+            CancellationToken cancellationToken)
+        {
+            var commandResult = await _sender.Send(command, cancellationToken);
+
+            return commandResult.IsSuccess ? NoContent() : commandResult.ToProblemDetails();
+        }
+    }
+}
