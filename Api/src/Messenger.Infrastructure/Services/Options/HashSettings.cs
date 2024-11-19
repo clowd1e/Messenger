c92769e15c12cc5defa3c7318c893d47089788f1
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-19 11:34:12 +0100

    Add options

---
+++ Api/src/Messenger.Infrastructure/Services/Options/HashSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.Infrastructure.Services.Options
+{
+    public sealed class HashSettings
+    {
+        public const string SectionName = nameof(HashSettings);
+
+        [Required]
+        [Range(1, 20)]
+        public int? WorkFactor { get; set; }
+    }
+}
