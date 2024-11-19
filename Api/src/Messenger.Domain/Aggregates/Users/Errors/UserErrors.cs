035cbd841071c9c83b8c84e3ca0f9b99cce25569
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-19 11:31:56 +0100

    Add new user errors

--- Api/src/Messenger.Domain/Aggregates/Users/Errors/UserErrors.cs
+++ Api/src/Messenger.Domain/Aggregates/Users/Errors/UserErrors.cs
@@ -13,5 +13,25 @@ namespace Messenger.Domain.Aggregates.User.Errors
             Error.Validation(
                 code: "User.UserAlreadyHasChat",
                 description: "User already has this chat.");
+
+        public static readonly Error InvalidAccessToken =
+            Error.Validation(
+                code: "User.InvalidAccessToken",
+                description: "The provided access token is invalid.");
+
+        public static readonly Error InvalidRefreshToken =
+            Error.Validation(
+                code: "User.InvalidRefreshToken",
+                description: "The provided refresh token is invalid.");
+
+        public static readonly Error RefreshTokenExpired =
+            Error.Validation(
+                code: "User.RefreshTokenExpired",
+                description: "The provided refresh token has expired.");
+
+        public static readonly Error InvalidCredentials =
+            Error.Validation(
+                code: "User.InvalidCredentials",
+                description: "The provided credentials are invalid.");
     }
 }
