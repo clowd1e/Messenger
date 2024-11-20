ca86f5434a9a7c7dec185b63af5127609c5de0fd
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-20 15:57:58 +0100

    Update chat-list component

--- Client/src/app/components/main/chat-list/chat-list.component.ts
+++ Client/src/app/components/main/chat-list/chat-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ChatItemComponent } from "../chat-item/chat-item.component";
+import { ChatItem } from '../../../Models/ChatItem';
 
 @Component({
   selector: 'app-chat-list',
@@ -9,11 +10,33 @@ import { ChatItemComponent } from "../chat-item/chat-item.component";
   styleUrl: './chat-list.component.scss'
 })
 export class ChatListComponent {
-  chatItems = [
-    { name: 'Chat 1', isChecked: false },
-    { name: 'Chat 2', isChecked: false },
-    { name: 'Chat 3', isChecked: false },
-    { name: 'Chat 4', isChecked: false },
-    { name: 'Chat 5', isChecked: false }
-  ]
+  chatList: Array<ChatItem> = [
+    { chatId: '1', chatCreationDate: new Date(), users: [
+      { userId: '1', userName: 'User 1', email: 'user1@gmail.com' },
+      { userId: '2', userName: 'User 2', email: 'user2@gmail.com' },
+      ], messages: [
+        { messageContent: 'Hello', messageTimestamp: new Date(2024, 11, 20, 12, 14, 32), senderId: '1' },
+        { messageContent: 'Hi', messageTimestamp: new Date(2024, 11, 20, 14, 12, 40), senderId: '2' },
+      ]
+    },
+    { chatId: '2', chatCreationDate: new Date(), users: [
+      { userId: '1', userName: 'User 1', email: 'user1@gmail.com' },
+      { userId: '3', userName: 'User 3', email: 'user3@gmail.com' },
+      ], messages: [
+        { messageContent: 'Hello', messageTimestamp: new Date(2024, 11, 20, 12, 14, 32), senderId: '1' },
+        { messageContent: 'Hi', messageTimestamp: new Date(2024, 11, 20, 14, 12, 40), senderId: '3' },
+        { messageContent: 'How are you?', messageTimestamp: new Date(2024, 11, 20, 14, 13, 43), senderId: '1' },
+        { messageContent: 'Well well well', messageTimestamp: new Date(2024, 11, 20, 14, 14, 50), senderId: '3' },
+      ]
+   },
+    { chatId: '3', chatCreationDate: new Date(), users: [
+      { userId: '1', userName: 'User 1', email: 'user1@gmail.com' },
+      { userId: '4', userName: 'User 4', email: 'user4@gmail.com' },
+      ], messages: [
+        { messageContent: 'Hello', messageTimestamp: new Date(2024, 11, 20, 12, 14, 32), senderId: '1' },
+        { messageContent: 'Hi', messageTimestamp: new Date(2024, 11, 20, 14, 12, 40), senderId: '4' },
+        { messageContent: 'Yes, I would certainly like to discuss that.', messageTimestamp: new Date(2024, 11, 20, 16, 13, 2), senderId: '4' },
+      ]
+    },
+  ];
 }
