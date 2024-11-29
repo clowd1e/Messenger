647fdba53b985bd15907aa0b6a1f722ceab41c4a
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-29 15:26:59 +0100

    Add ChatHub

---
+++ Api/src/Messenger.WebAPI/Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Messenger.Application.Features.Users.Queries.GetCurrentUserChats;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Messenger.WebAPI.Hubs
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public sealed class ChatHub : Hub
+    {
+        private readonly ISender _sender;
+
+        public ChatHub(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var queryResult = await _sender.Send(new GetCurrentUserChatsQuery());
+
+            if (queryResult.IsFailure)
+            {
+                await Clients.Caller.SendAsync("ReceiveError", queryResult.Error);
+                return;
+            }
+
+            var chats = queryResult.Value;
+
+            await Clients.Caller.SendAsync("ReceiveUserChats", chats);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            
+        }
+    }
+}
