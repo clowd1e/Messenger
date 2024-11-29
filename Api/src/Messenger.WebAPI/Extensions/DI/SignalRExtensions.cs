ca05a03e7a314aea6dad7ce9b3fe5c5702820b39
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-11-29 15:27:17 +0100

    Configure SignalR

---
+++ Api/src/Messenger.WebAPI/Extensions/DI/SignalRExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Messenger.WebAPI.Hubs;
+
+namespace Messenger.WebAPI.Extensions.DI
+{
+    public static class SignalRExtensions
+    {
+        public static IServiceCollection ConfigureSignalR(this IServiceCollection services)
+        {
+            services.AddSignalR(options =>
+            {
+                options.KeepAliveInterval = TimeSpan.FromSeconds(10);
+            });
+
+            return services;
+        }
+
+        public static IEndpointRouteBuilder MapSignalRHubs(this IEndpointRouteBuilder app)
+        {
+            app.MapHub<ChatHub>("/hubs/chat");
+
+            return app;
+        }
+    }
+}
