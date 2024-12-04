8e3cdfd86a8c8c8ddd5744de367f95d3acaac68f
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-12-04 18:33:09 +0100

    Add chat component

--- Client/src/app/components/main/chat/chat.component.ts
+++ Client/src/app/components/main/chat/chat.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ChatInputComponent } from "../chat-input/chat-input.component";
+import { ChatMessageListComponent } from "../chat-message-list/chat-message-list.component";
+import { CommonModule } from '@angular/common';
+import { ChatItem } from '../../../Models/ChatItem';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ChatInputComponent, ChatMessageListComponent, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  chat = input<ChatItem>();
 
+  getMessages() {
+    return this.chat()?.messages || [];
+  }
 }
