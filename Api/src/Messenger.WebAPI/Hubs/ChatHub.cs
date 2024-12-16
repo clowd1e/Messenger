83fe5906b1e5b907741c8b7c8f324eba2947ab52
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-12-16 12:55:01 +0100

    Implement CreateChat Hub method

--- Api/src/Messenger.WebAPI/Hubs/ChatHub.cs
+++ Api/src/Messenger.WebAPI/Hubs/ChatHub.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
+using Messenger.Application.Features.Chats.Commands.Create;
 using Messenger.Application.Features.Chats.Commands.SendMessage;
+using Messenger.Application.Features.Chats.Queries.GetById;
 using Messenger.Application.Features.Users.Queries.GetCurrentUserChats;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -30,6 +32,32 @@ namespace Messenger.WebAPI.Hubs
             var chats = queryResult.Value;
 
             await Clients.Caller.ReceiveUserChats(chats);
+        }
+
+        public async Task CreateChat(CreateChatCommand command)
+        {
+            var commandResult = await _sender.Send(command);
+
+            if (commandResult.IsFailure)
+            {
+                await Clients.Caller.ReceiveError(commandResult.Error);
+                return;
+            }
+
+            var chatId = commandResult.Value;
+
+            var queryResult = await _sender.Send(new GetChatByIdQuery(chatId));
+
+            if (queryResult.IsFailure)
+            {
+                await Clients.Caller.ReceiveError(queryResult.Error);
+                return;
+            }
+
+            var chat = queryResult.Value;
+
+            await Clients.User(Context.ConnectionId).ReceiveChat(chat);
+            await Clients.User(command.InviteeId.ToString()).ReceiveChat(chat);
         }
 
         public async Task SendMessage(SendMessageCommand command)
