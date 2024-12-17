dc4195aa75d3862a7f553edb181507888026d53a
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-12-17 15:53:24 +0100

    Implement GetAllUsers (#22)
    * Implement GetAllUsers use case
    * Implement GetAllUsers endpoint

---
+++ Api/src/Messenger.Application/Features/Users/Queries/GetAll/GetAllUsersQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Messenger.Application.Features.Users.DTO;
+
+namespace Messenger.Application.Features.Users.Queries.GetAll
+{
+    public sealed record GetAllUsersQuery() 
+        : IRequest<Result<IEnumerable<UserResponse>>>;
+}
---
+++ Api/src/Messenger.Application/Features/Users/Queries/GetAll/GetAllUsersQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Messenger.Application.Abstractions.Data;
+using Messenger.Application.Features.Users.DTO;
+using Messenger.Domain.Aggregates.Users;
+
+namespace Messenger.Application.Features.Users.Queries.GetAll
+{
+    internal sealed class GetAllUsersQueryHandler
+        : IRequestHandler<GetAllUsersQuery, Result<IEnumerable<UserResponse>>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly Mapper<User, UserResponse> _userMapper;
+
+        public GetAllUsersQueryHandler(
+            IUserRepository userRepository,
+            Mapper<User, UserResponse> userMapper)
+        {
+            _userRepository = userRepository;
+            _userMapper = userMapper;
+        }
+
+        public async Task<Result<IEnumerable<UserResponse>>> Handle(
+            GetAllUsersQuery request,
+            CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetAllAsync(cancellationToken);
+
+            var usersMap = _userMapper.Map(users);
+
+            return Result.Success(usersMap);
+        }
+    }
+}
