064de25ebee140addd93b394e1605a9e67ad152c
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-01-05 00:24:14 +0100

    Implement SetUserIcon use case

---
+++ Api/src/Messenger.Application/Features/Users/Commands/SetIcon/SetUserIconCommandHandler.cs
@@ -0,0 +1,86 @@
+﻿using MediatR;
+using Messenger.Application.Abstractions.Data;
+using Messenger.Application.Abstractions.Identity;
+using Messenger.Application.Abstractions.Storage;
+using Messenger.Domain.Aggregates.User.Errors;
+using Messenger.Domain.Aggregates.Users;
+using Messenger.Domain.Aggregates.Users.ValueObjects;
+using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp;
+using System.Drawing;
+
+namespace Messenger.Application.Features.Users.Commands.SetIcon
+{
+    internal sealed class SetUserIconCommandHandler
+        : IRequestHandler<SetUserIconCommand, Result>
+    {
+        private readonly IImageService _imageService;
+        private readonly IUserRepository _userRepository;
+        private readonly IUserContextService<Guid> _userContextService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SetUserIconCommandHandler(
+            IImageService imageService,
+            IUserRepository userRepository,
+            IUserContextService<Guid> userContextService,
+            IUnitOfWork unitOfWork)
+        {
+            _imageService = imageService;
+            _userRepository = userRepository;
+            _userContextService = userContextService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(
+            SetUserIconCommand request,
+            CancellationToken cancellationToken)
+        {
+            var userId = new UserId(_userContextService.GetAuthenticatedUserId());
+
+            var user = await _userRepository.GetByIdAsync(userId, cancellationToken);
+
+            if (user is null)
+            {
+                return UserErrors.NotFound;
+            }
+
+            var iconDimensionsValid = IconDimensionsValid(request.Icon!);
+
+            if (!iconDimensionsValid)
+            {
+                return UserErrors.InvalidIconDimensions;
+            }
+
+            if (user.IconUri is not null)
+            {
+                await _imageService.DeleteImageAsync(
+                    user.IconUri, cancellationToken);
+            }
+
+            var iconUri = await _imageService.UploadImageAsync(
+                request.Icon, cancellationToken);
+
+            user.SetIconUri(iconUri);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+
+        private bool IconDimensionsValid(IFormFile icon)
+        {
+            (int width, int height) = GetImageDimensions(icon);
+
+            return width == height;
+        }
+
+        private (int Width, int Height) GetImageDimensions(IFormFile file)
+        {
+            using var stream = file.OpenReadStream();
+
+            using var image = Image.Load(stream);
+
+            return (image.Width, image.Height);
+        }
+    }
+}
---
+++ Api/src/Messenger.Application/Features/Users/Commands/SetIcon/SetUserIconCommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Messenger.Application.Images.Options;
+using Microsoft.Extensions.Options;
+
+namespace Messenger.Application.Features.Users.Commands.SetIcon
+{
+    internal sealed class SetUserIconCommandValidator
+        : AbstractValidator<SetUserIconCommand>
+    {
+        private const int MaxIconLengthInBytes = 250_000;
+        
+        private readonly ImageSettings _imageSettings;
+
+        public SetUserIconCommandValidator(
+            IOptions<ImageSettings> imageSettings)
+        {
+            _imageSettings = imageSettings.Value;
+
+            RuleFor(x => x.Icon)
+                .NotEmpty()
+                .WithMessage("Icon is required.");
+
+            RuleFor(x => x.Icon.ContentType)
+                .Must(ValidImageType)
+                .When(x => x.Icon is not null)
+                .WithMessage($"Icon format should be one of {GetAllowedContentTypes()}.");
+
+            RuleFor(x => x.Icon.Length)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(MaxIconLengthInBytes)
+                .When(x => x.Icon is not null)
+                .WithMessage("Icon size is invalid.");
+        }
+
+        private bool ValidImageType(string contentType) =>
+            _imageSettings.AllowedContentTypes.Contains(contentType);
+
+        private string GetAllowedContentTypes() =>
+            string.Join(", ", _imageSettings.AllowedContentTypes
+                .Select(type => $".{type.Split('/').Last()}"));
+    }
+}
