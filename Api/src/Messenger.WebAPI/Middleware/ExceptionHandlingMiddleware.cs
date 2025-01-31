d43f78d7e20b3ea89d727109157caf43eecaa83d
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-02-01 00:56:53 +0100

    Provide Logger for ExceptionHandlingMiddleware (#44)

--- Api/src/Messenger.WebAPI/Middleware/ExceptionHandlingMiddleware.cs
+++ Api/src/Messenger.WebAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -5,10 +5,14 @@ namespace Messenger.WebAPI.Middleware
     public sealed class ExceptionHandlingMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
 
-        public ExceptionHandlingMiddleware(RequestDelegate next)
+        public ExceptionHandlingMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionHandlingMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -23,10 +27,12 @@ namespace Messenger.WebAPI.Middleware
             }
         }
 
-        private static async Task HandleExceptionAsync(
+        private async Task HandleExceptionAsync(
             HttpContext context,
             Exception exception)
         {
+            _logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
             var problemDetails = new ProblemDetails
             {
                 Status = StatusCodes.Status500InternalServerError,
