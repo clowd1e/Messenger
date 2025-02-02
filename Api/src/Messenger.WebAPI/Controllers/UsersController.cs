b4be6b3577f3b73641f531a17d679a8b30c2f6d5
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-02-02 22:43:24 +0100

    Implement GetAllUsersExceptCurrent endpoint

--- Api/src/Messenger.WebAPI/Controllers/UsersController.cs
+++ Api/src/Messenger.WebAPI/Controllers/UsersController.cs
@@ -2,14 +2,17 @@
 using Messenger.Application.Features.Users.Commands.RemoveIcon;
 using Messenger.Application.Features.Users.Commands.SetIcon;
 using Messenger.Application.Features.Users.Queries.GetAll;
+using Messenger.Application.Features.Users.Queries.GetAllExceptCurrent;
 using Messenger.Application.Features.Users.Queries.GetById;
 using Messenger.WebAPI.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Messenger.WebAPI.Controllers
 {
     [Route("api/users")]
     [ApiController]
+    [Authorize]
     public sealed class UsersController : ControllerBase
     {
         private readonly ISender _sender;
@@ -32,6 +35,14 @@ namespace Messenger.WebAPI.Controllers
         public async Task<IActionResult> GetAllUsers()
         {
             var queryResult = await _sender.Send(new GetAllUsersQuery());
+
+            return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
+        }
+
+        [HttpGet("except-current")]
+        public async Task<IActionResult> GetAllUsersExceptCurrent()
+        {
+            var queryResult = await _sender.Send(new GetAllUsersExceptCurrentQuery());
 
             return queryResult.IsSuccess ? Ok(queryResult.Value) : queryResult.ToProblemDetails();
         }
