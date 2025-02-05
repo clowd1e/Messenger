49bedca0830f9813ab1d871f160eaff1fa8a4144
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-02-05 22:12:14 +0100

    Update production configuration

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_BASE_URL: 'https://messenger-api-ayfvayf0cvethjcm.polandcentral-01.azurewebsites.net/api',
-    HUB_BASE_URL: 'https://messenger-api-ayfvayf0cvethjcm.polandcentral-01.azurewebsites.net/hubs'
+    API_BASE_URL: 'https://messengerm-api-c0fackcdh2emf4d9.polandcentral-01.azurewebsites.net/api',
+    HUB_BASE_URL: 'https://messengerm-api-c0fackcdh2emf4d9.polandcentral-01.azurewebsites.net/hubs'
 };
