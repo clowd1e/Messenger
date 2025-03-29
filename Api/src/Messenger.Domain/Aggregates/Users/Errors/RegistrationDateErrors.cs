5d4671721d485e2ab8575e3381c6e7f3958e9e4d
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-03-29 18:07:17 +0100

    Add RegistrationDate and EmailConfirmed properties

---
+++ Api/src/Messenger.Domain/Aggregates/Users/Errors/RegistrationDateErrors.cs
@@ -0,0 +1,12 @@
+﻿using Messenger.Domain.Shared;
+
+namespace Messenger.Domain.Aggregates.Users.Errors
+{
+    public static class RegistrationDateErrors
+    {
+        public static Error FutureDate =
+            Error.Validation(
+                code: "RegistrationDate.FutureDate",
+                description: "Registration date cannot be in the future.");
+    }
+}
