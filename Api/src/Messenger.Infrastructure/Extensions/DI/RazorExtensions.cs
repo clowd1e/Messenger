daec40b0894c3c071581fd9c984b74f32d54070d
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-03-29 18:14:49 +0100

    Add razor class library project for email templates

---
+++ Api/src/Messenger.Infrastructure/Extensions/DI/RazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger.Infrastructure.Extensions.DI
+{
+    public static class RazorExtensions
+    {
+        public static IServiceCollection AddAppRazorTemplating(this IServiceCollection services)
+        {
+            services.AddRazorTemplating();
+
+            return services;
+        }
+    }
+}
