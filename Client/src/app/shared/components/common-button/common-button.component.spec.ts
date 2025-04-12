a220d8611611789bace4ba1f7593c6edb4138158
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-04-12 21:29:06 +0200

    Rename add-chat-button to common-button

--- Client/src/app/shared/components/common-button/common-button.component.spec.ts
+++ Client/src/app/shared/components/common-button/common-button.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddChatButtonComponent } from './add-chat-button.component';
+import { CommonButtonComponent } from './common-button.component';
 
-describe('AddChatButtonComponent', () => {
-  let component: AddChatButtonComponent;
-  let fixture: ComponentFixture<AddChatButtonComponent>;
+describe('CommonButtonComponent', () => {
+  let component: CommonButtonComponent;
+  let fixture: ComponentFixture<CommonButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddChatButtonComponent]
+      imports: [CommonButtonComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AddChatButtonComponent);
+    fixture = TestBed.createComponent(CommonButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- Client/src/app/shared/components/common-button/common-button.component.ts
+++ Client/src/app/shared/components/common-button/common-button.component.ts
@@ -2,28 +2,30 @@ import { CommonModule } from '@angular/common';
 import { Component, input, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-add-chat-button',
+  selector: 'app-common-button',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './add-chat-button.component.html',
-  styleUrl: './add-chat-button.component.scss'
+  templateUrl: './common-button.component.html',
+  styleUrl: './common-button.component.scss'
 })
-export class AddChatButtonComponent {
+export class CommonButtonComponent {
   @Input() borderRadius: number = 0;
   @Input() textColor: string = '#000';
   @Input() backgroundColor: string = '#6799EF';
   @Input() fontSize: number = 12;
   @Input() padding: string = '0px';
-
+  @Input() fontWeight: number = 400;
+  
   text = input.required<string>();
-
+  
   getStyles() {
     return {
       'color': this.textColor,
       'background-color': this.backgroundColor,
       'border-radius': `${this.borderRadius}px`,
       'padding': this.padding,
-      'font-size': `${this.fontSize}px`
+      'font-size': `${this.fontSize}px`,
+      'font-weight': this.fontWeight,
     };
   }
 }
