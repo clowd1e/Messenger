c9d2d28eade6423c3254feebe2afed973bc596e8
clowd1e <sashapro712@gmail.com>
2025-05-05 15:30:14 +0200

    Add new migration

---
+++ Api/src/Messenger.Infrastructure/Migrations/20250504223421_ResetPasswordToken.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResetPasswordToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "reset_password_token",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    token_hash = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    expires_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_used = table.Column<bool>(type: "bit", nullable: false),
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reset_password_token", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_reset_password_token_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reset_password_token_id",
+                table: "reset_password_token",
+                column: "id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reset_password_token_user_id",
+                table: "reset_password_token",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reset_password_token");
+        }
+    }
+}
