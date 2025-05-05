eea993f1ad76d3846ae1e5bb9a6a04b45739e589
clowd1e <sashapro712@gmail.com>
2025-05-05 15:32:36 +0200

    Add ResetPasswordTokenSettings

---
+++ Api/src/Messenger.Application/Identity/Options/ResetPasswordTokenSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.Application.Identity.Options
+{
+    public sealed class ResetPasswordTokenSettings
+    {
+        public const string SectionName = nameof(ResetPasswordTokenSettings);
+
+        [Required]
+        [Range(1, 12)]
+        public int ExpirationTimeInHours { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int ActiveTokensLimit { get; set; }
+    }
+}
