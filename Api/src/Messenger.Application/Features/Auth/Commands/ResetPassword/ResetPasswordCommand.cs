f905cf271e821fda2e800c96685276686b4198b4
clowd1e <sashapro712@gmail.com>
2025-05-05 15:35:00 +0200

    Implement ResetPassword use case

---
+++ Api/src/Messenger.Application/Features/Auth/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Messenger.Application.Features.Auth.Commands.ResetPassword
+{
+    public sealed record ResetPasswordCommand(
+        Guid UserId,
+        Guid TokenId,
+        string Token,
+        string NewPassword) : IRequest<Result>;
+}
---
+++ Api/src/Messenger.Application/Features/Auth/Commands/ResetPassword/ResetPasswordCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Messenger.Application.Extensions.Validation;
+
+namespace Messenger.Application.Features.Auth.Commands.ResetPassword
+{
+    internal sealed class ResetPasswordCommandValidator
+        : AbstractValidator<ResetPasswordCommand>
+    {
+        public ResetPasswordCommandValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty();
+
+            RuleFor(x => x.TokenId)
+                .NotEmpty();
+
+            RuleFor(x => x.Token)
+                .NotEmpty();
+
+            RuleFor(x => x.NewPassword)
+                .ValidatedPassword();
+        }
+    }
+}
