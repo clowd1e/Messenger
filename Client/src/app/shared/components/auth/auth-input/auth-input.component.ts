482c9125b814accb722929b9b73b8f335f7d93d7
clowd1e <sashapro712@gmail.com>
2025-07-31 12:34:59 +0200

    Add password visibility toggle

--- Client/src/app/shared/components/auth/auth-input/auth-input.component.ts
+++ Client/src/app/shared/components/auth/auth-input/auth-input.component.ts
@@ -1,7 +1,7 @@
-import { NgTemplateOutlet } from '@angular/common';
-import { Component, forwardRef, input, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { Component, forwardRef, input } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SvgConfiguration } from '../../../models/configurations/UI/svg-configuration';
+import { eyeIconPath, eyeSlashIconPath } from '../icons/icons';
 
 @Component({
   selector: 'app-auth-input',
@@ -18,6 +18,11 @@ import { SvgConfiguration } from '../../../models/configurations/UI/svg-configur
     ]
 })
 export class AuthInputComponent {
+  private passwordVisible = false;
+  private eyeIconPath = eyeIconPath;
+  private eyeSlashIconPath = eyeSlashIconPath;
+  
+  name = input.required<string>();
   inputType = input.required<string>();
   svgConfig = input.required<SvgConfiguration>();
   inputPlaceholder = input.required<string>();
@@ -45,5 +50,25 @@ export class AuthInputComponent {
     const target = event.target as HTMLInputElement;
     this.value = target.value;
     this.onChange(this.value);
+  }
+
+  get isPasswordInput(): boolean {
+    return this.inputType() === 'password';
+  }
+
+  get isPasswordVisible(): boolean {
+    return this.passwordVisible;
+  }
+
+  get getInputType(): string {
+    return this.isPasswordVisible ? 'text' : this.inputType();
+  }
+
+  get passwordIcon(): string {
+    return this.isPasswordVisible ? this.eyeSlashIconPath : this.eyeIconPath;
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
   }
 }
