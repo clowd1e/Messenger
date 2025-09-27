ddf510aa97cd5e0c9e33b58f343ad68fa39f98fe
clowd1e <sashapro712@gmail.com>
2025-09-28 00:28:45 +0200

    Add custom json multipart data support

---
+++ Api/src/Messenger.WebAPI/MultipartJsonSupport/FormDataJsonBinder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json;
+
+namespace Messenger.WebAPI.MultipartJsonSupport
+{
+    public class FormDataJsonBinder : IModelBinder
+    {
+        private readonly static JsonSerializerOptions _options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var modelName = bindingContext.ModelName;
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+                return Task.CompletedTask;
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var json = valueProviderResult.FirstValue;
+
+            if (string.IsNullOrWhiteSpace(json))
+                return Task.CompletedTask;
+
+            try
+            {
+                var model = JsonSerializer.Deserialize(json, bindingContext.ModelType, _options);
+
+                bindingContext.Result = ModelBindingResult.Success(model);
+            }
+            catch (JsonException ex)
+            {
+                bindingContext.ModelState.TryAddModelError(modelName, ex, bindingContext.ModelMetadata);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
