de7f6e4cdbab7232632e7ceacb4bc84fda0d00b0
clowd1e <sashapro712@gmail.com>
2025-10-12 19:39:09 +0200

    Add Azure Function project

---
+++ Api/src/Messenger.AzureFunctions/Settings/CronExpressionAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.AzureFunctions.Settings
+{
+    public sealed class CronExpressionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not string cronExpression || string.IsNullOrWhiteSpace(cronExpression))
+            {
+                return new ValidationResult("Cron expression must be a non-empty string.");
+            }
+
+            var parts = cronExpression.Split(' ');
+
+            if (parts.Length < 5 || parts.Length > 6)
+            {
+                return new ValidationResult("Cron expression must have 5 or 6 parts.");
+            }
+
+            foreach (var part in parts)
+            {
+                if (string.IsNullOrWhiteSpace(part))
+                {
+                    return new ValidationResult("Each part of the cron expression must be non-empty.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Api/src/Messenger.AzureFunctions/Settings/TimeTriggerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.AzureFunctions.Settings
+{
+    public sealed class TimeTriggerSettings
+    {
+        public const string SectionName = nameof(TimeTriggerSettings);
+
+        [Required]
+        [CronExpression]
+        public required string RefreshTokenCleanupSchedule { get; set; }
+    }
+}
